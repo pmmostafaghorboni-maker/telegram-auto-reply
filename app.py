5a404801695cc928964cabe5df5e7a05e1cae7c5
pmmostafaghorboni-maker <p.m.mostafaghorboni@gmail.com>
2026-09-14 20:46:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 ADMIN_ID = "6600182795"
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 AUTO_REPLY_TEXT = "Hi! I'm not available right now, but I'll get back to you as soon as possible.✨"
-COOLDOWN = 24 * 60 * 60  # 24 hours
+COOLDOWN = 1  # 24 hours
 
 # 📞 اطلاعات تماس شما
 PHONE_NUMBER = "+989058407880"
