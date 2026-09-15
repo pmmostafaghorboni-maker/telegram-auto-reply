7801499a9410e914090a372cf558f77ca9c79f31
pmmostafaghorboni-maker <p.m.mostafaghorboni@gmail.com>
2026-09-15 16:17:25 +0330

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 ADMIN_ID = "6600182795"
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 AUTO_REPLY_TEXT = "Hi! I'm not available right now, but I'll get back to you as soon as possible.✨"
-COOLDOWN = 24 * 60 * 60  # 24 hours
+COOLDOWN = 1 #24 * 60 * 60  # 24 hours
 AUTO_DELETE_SECONDS = 300  # ۵ دقیقه (برای تست: 10)
 
 # 📞 اطلاعات تماس شما
